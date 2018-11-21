d03f46909b1a34b973d5d234b7b0c44b15592be8
yaoyang <kiki@smarty.so>
2018-11-21 15:34:52 +0800

    去掉测试逻辑

--- command/spider_job/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-spider_job('test', '* * * * *', 'http://127.0.0.1/test', [
-    'name'         =>  'n',
-    'content.0.asd'  =>  'd.a.b',
-]);
--- controller/index.php
+++ controller/index.php
@@ -2,33 +2,5 @@
 
 if_get('/', function ()
 {
-    $res = spider_last_data_query('test');
-    echo "<pre>";
-    var_dump($res);exit;
-
     return 'hello world';
-});
-
-if_get('/test', function () {
-
-    return [
-        'name' => 'name1',
-        'sex' => 'name2',
-        'age' => 'name3',
-        'content' => [
-            [
-                'asd' => 'bcd',
-            ],
-            [
-                'asd' => 'bcd',
-            ],
-            [
-                'asd' => 'bcd',
-            ],
-            [
-                'asd' => 'bcd',
-            ],
-        ],
-        'description' => 'name5',
-    ];
 });
