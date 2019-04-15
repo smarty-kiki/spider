3ca5178f547f13c741d60a7f0a01f97e98a95c6b
yaoyang <kiki@smarty.so>
2019-04-15 13:55:02 +0800

    spider 开发环境模拟 crontab

--- project/tool/start_dev_server.sh
+++ project/tool/start_dev_server.sh
@@ -9,6 +9,7 @@ sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name spider \
     -v $ROOT_DIR/:/var/www/spider \
     -v $ROOT_DIR/project/config/development/nginx/spider.conf:/etc/nginx/sites-enabled/default \
     -v $ROOT_DIR/project/config/development/supervisor/spider_worker.conf:/etc/supervisor/conf.d/spider_worker.conf \
+    -v $ROOT_DIR/project/config/development/supervisor/mock_crontab.conf:/etc/supervisor/conf.d/mock_crontab.conf \
     -e 'TIMEZONE=Asia/Shanghai' \
     -e 'AFTER_START_SHELL=/var/www/spider/project/tool/after_dev_env_start.sh' \
 kikiyao/debian_php_dev_env start
