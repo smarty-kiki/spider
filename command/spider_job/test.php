8ad097a78e7383ecb4f25f108758f9bf8994a2ad
yaoyang <kiki@smarty.so>
2019-04-15 14:27:13 +0800

    demo 添加数据预处理逻辑

--- command/spider_job/test.php
+++ command/spider_job/test.php
@@ -3,4 +3,9 @@
 spider_job_get('demo', '* * * * *', 'http://127.0.0.1/target', 'json', [
     'test' => 'a',
     'test3.a' => 'b',
-]);
+], function ($data) {
+
+    $data['c'] = datetime();
+
+    return $data;
+});
