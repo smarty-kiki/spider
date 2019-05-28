ae7991e4a752ff9298f610e5a7d7a942cf73e01e
yaoyang <kiki@smarty.so>
2019-05-28 10:26:11 +0800

    debug naming_project

--- project/tool/naming_project.sh
+++ project/tool/naming_project.sh
@@ -17,6 +17,7 @@ mv $ROOT_DIR/project/config/production/nginx/spider_frame.conf $ROOT_DIR/project
 
 sed_name $ROOT_DIR/project/config/development/nginx/$1.conf $1
 sed_name $ROOT_DIR/project/config/development/supervisor/spider_worker.conf $1
+sed_name $ROOT_DIR/project/config/development/supervisor/mock_crontab.conf $1
 sed_name $ROOT_DIR/project/config/production/nginx/$1.conf $1
 sed_name $ROOT_DIR/project/config/production/supervisor/spider_worker.conf $1
 sed_name $ROOT_DIR/project/tool/start_dev_server.sh $1
