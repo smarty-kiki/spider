7b80ebd310fe23b809b5c0386a36744be2bd276e
yaoyang <kiki@smarty.so>
2019-10-08 10:54:03 +0800

    添加初次安装的 migrate:install 命令

--- project/tool/after_push.sh
+++ project/tool/after_push.sh
@@ -6,6 +6,7 @@ ln -fs $ROOT_DIR/project/config/production/nginx/spider_frame.conf /etc/nginx/si
 /usr/sbin/service nginx reload
 
 /bin/bash $ROOT_DIR/project/tool/dep_build.sh link
+/usr/bin/php $ROOT_DIR/public/cli.php migrate:install
 /usr/bin/php $ROOT_DIR/public/cli.php migrate
 
 ln -fs $ROOT_DIR/project/config/production/supervisor/spider_worker.conf /etc/supervisor/conf.d/spider_worker.conf
